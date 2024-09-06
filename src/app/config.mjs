e8f942b99204a007734a477557e279c3e4213317
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-09-06 15:45:50 -0400

    fix: Commit missing files

---
+++ src/app/config.mjs
@@ -0,0 +1,84 @@
+export const appConfig = {
+  appName: "NFT to the Future",
+  themeColor: "#FFFFFF",
+  icons: [
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "57x57",
+      url: "/apple-touch-icon-57x57.png",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "114x114",
+      url: "/apple-touch-icon-114x114.png",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "72x72",
+      url: "/apple-touch-icon-72x72.png",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "144x144",
+      url: "/apple-touch-icon-144x144.png",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "60x60",
+      url: "/apple-touch-icon-60x60.png",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "120x120",
+      url: "/apple-touch-icon-120x120.png",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "76x76",
+      url: "/apple-touch-icon-76x76.png",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      sizes: "152x152",
+      url: "/apple-touch-icon-152x152.png",
+    },
+    {
+      rel: "icon",
+      sizes: "196x196",
+      type: "image/png",
+      url: "/favicon-196x196.png",
+    },
+    {
+      rel: "icon",
+      sizes: "96x96",
+      type: "image/png",
+      url: "/favicon-96x96.png",
+    },
+    {
+      rel: "icon",
+      sizes: "32x32",
+      type: "image/png",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      sizes: "16x16",
+      type: "image/png",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      sizes: "128x128",
+      type: "image/png",
+      url: "/favicon-128.png",
+    },
+  ],
+  msapplication: {
+    "msapplication-TileColor": "#FFFFFF",
+    "msapplication-TileImage": "/mstile-144x144.png",
+    "msapplication-square70x70logo": "/mstile-70x70.png",
+    "msapplication-square150x150logo": "/mstile-150x150.png",
+    "msapplication-wide310x150logo": "/mstile-310x150.png",
+    "msapplication-square310x310logo": "/mstile-310x310.png",
+  },
+};
---
+++ src/components/TimeAxis.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const TimeAxis = () => {
+  // Array representing the months for labels
+
+  return (
+    <div className="relative h-full flex flex-col justify-center items-end space-y-2">
+      {Array.from({ length: 30 }, (_, i) => (
+        <motion.div
+          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+          key={i}
+          className={`w-1 ${
+            i % 5 === 0 ? "h-6 w-8 bg-gray-700" : "h-6 w-6 bg-gray-500"
+          }`}
+          initial={{ y: 100 }}
+          animate={{ y: -200 }}
+          transition={{
+            // biome-ignore lint/style/useNumberNamespace: <explanation>
+            repeat: Infinity,
+            duration: 5, // You can adjust this duration
+            delay: i * 0.1,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TimeAxis;
